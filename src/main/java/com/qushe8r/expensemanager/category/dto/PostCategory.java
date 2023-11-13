c88ceb16daaa9199f89373d6841be3fc2212f92c
qushe8r <qushe8r@gmail.com>
2023-11-13 23:28:46 +0900

    Lint: spotless 적용 #8

--- src/main/java/com/qushe8r/expensemanager/category/dto/PostCategory.java
+++ src/main/java/com/qushe8r/expensemanager/category/dto/PostCategory.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record PostCategory(
-    @NotNull
-        @Pattern(regexp = "^[가-힣]*$", message = "한글만 사용할 수 있습니다.")
+    @NotNull @Pattern(regexp = "^[가-힣]*$", message = "한글만 사용할 수 있습니다.")
         @Size(min = 1, max = 10, message = "1글자 이상, 10글자 이하여야 합니다.")
         String name) {}
