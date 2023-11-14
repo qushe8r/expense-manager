6f24bd2d7863fa18f19540709bdbffc81367570f
qushe8r <qushe8r@gmail.com>
2023-11-14 17:27:52 +0900

    Remove: PostBudgetCategory 제거 #2

--- src/main/java/com/qushe8r/expensemanager/budget/dto/PostBudget.java
+++ src/main/java/com/qushe8r/expensemanager/budget/dto/PostBudget.java
@@ -1,6 +1,5 @@
 package com.qushe8r.expensemanager.budget.dto;
 
-import com.qushe8r.expensemanager.category.dto.PostBudgetCategory;
 import java.time.YearMonth;
 
-public record PostBudget(Long budget, YearMonth month, PostBudgetCategory category) {}
+public record PostBudget(Long budget, YearMonth month, Long categoryId) {}
--- src/main/java/com/qushe8r/expensemanager/category/dto/PostBudgetCategory.java
+++
@@ -1,3 +0,0 @@
-package com.qushe8r.expensemanager.category.dto;
-
-public record PostBudgetCategory(Long id, String name) {}
