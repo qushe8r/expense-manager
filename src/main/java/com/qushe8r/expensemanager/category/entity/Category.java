b8fac3cf83dd3f75d3d38a63bd521954ad0ef7f4
qushe8r <qushe8r@gmail.com>
2023-11-14 13:46:48 +0900

    Feat: Entity 연관 관계 정리, 생성자 추가 #2

--- src/main/java/com/qushe8r/expensemanager/category/entity/Category.java
+++ src/main/java/com/qushe8r/expensemanager/category/entity/Category.java
@@ -5,24 +5,31 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter(AccessLevel.PRIVATE)
 @Entity
 @Table(name = "category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "category_id")
   private Long id;
 
   @Column(unique = true, updatable = false)
   private String name;
+
+  @OneToMany(mappedBy = "category")
+  private List<MemberCategory> memberCategory;
 
   public Category(Long id, String name) {
     this.id = id;
