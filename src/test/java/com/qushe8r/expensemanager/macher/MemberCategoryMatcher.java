b959264986698b579bbff4259e6b83f22ba11858
qushe8r <qushe8r@gmail.com>
2023-11-14 17:29:11 +0900

    Feat: MemberCategoryMatcher 추가 #2

---
+++ src/test/java/com/qushe8r/expensemanager/macher/MemberCategoryMatcher.java
@@ -0,0 +1,31 @@
+package com.qushe8r.expensemanager.macher;
+
+import com.qushe8r.expensemanager.category.entity.MemberCategory;
+import org.mockito.ArgumentMatcher;
+
+public class MemberCategoryMatcher implements ArgumentMatcher<MemberCategory> {
+
+  private final MemberCategory left;
+
+  public MemberCategoryMatcher(MemberCategory left) {
+    this.left = left;
+  }
+
+  @Override
+  public boolean matches(MemberCategory right) {
+    boolean isSameId = isSameId(left.getId(), right.getId());
+    boolean isSameCategoryId = isSameId(left.getCategory().getId(), right.getCategory().getId());
+    boolean isSameMemberId = isSameId(left.getMember().getId(), right.getMember().getId());
+    return isSameId && isSameCategoryId && isSameMemberId;
+  }
+
+  private boolean isSameId(Long left, Long right) {
+    if (left == null && right == null) {
+      return true;
+    }
+    if (left != null && right != null) {
+      return left.equals(right);
+    }
+    return false;
+  }
+}
