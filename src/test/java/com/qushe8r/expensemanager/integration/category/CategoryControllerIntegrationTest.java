ef7b76d4449d4c3d3eb81e3f664ff7811c0b8cbb
qushe8r <qushe8r@gmail.com>
2023-11-17 19:41:00 +0900

    Test: GET /Categories 통합 테스트

--- src/test/java/com/qushe8r/expensemanager/integration/category/CategoryControllerIntegrationTest.java
+++ src/test/java/com/qushe8r/expensemanager/integration/category/CategoryControllerIntegrationTest.java
@@ -2,12 +2,13 @@ package com.qushe8r.expensemanager.integration.category;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qushe8r.expensemanager.category.dto.PostCategory;
-import com.qushe8r.expensemanager.security.filter.JwtAuthenticationFilter;
-import com.qushe8r.expensemanager.security.filter.JwtVerificationFilter;
+import com.qushe8r.expensemanager.category.entity.Category;
+import com.qushe8r.expensemanager.category.repository.CategoryRepository;
 import com.qushe8r.expensemanager.security.jwt.JwtProperties;
 import com.qushe8r.expensemanager.security.jwt.TokenProvider;
 import com.qushe8r.expensemanager.stub.JwtFactory;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,8 @@ class CategoryControllerIntegrationTest {
   @Autowired private ObjectMapper objectMapper;
 
   @Autowired private JwtProperties jwtProperties;
+
+  @Autowired private CategoryRepository categoryRepository;
 
   @DisplayName("createCategoryTokenNotFound(): 토큰이 없으면 실패한다.")
   @Test
@@ -86,5 +89,32 @@ class CategoryControllerIntegrationTest {
     actions
         .andDo(MockMvcResultHandlers.print())
         .andExpect(MockMvcResultMatchers.status().isCreated());
+  }
+
+  @DisplayName("getCategories(): 조회 성공시 카테고리 목록을 반환한다.")
+  @Test
+  void getCategories() throws Exception {
+    // given
+    List<Category> response = List.of(new Category(CATEGORY_NAME));
+    categoryRepository.saveAll(response);
+    String accessToken = JwtFactory.withDefaultValues().generateToken(jwtProperties);
+
+    // when
+    ResultActions actions =
+        mockMvc.perform(
+            MockMvcRequestBuilders.get(CATEGORY_DEFAULT_URL)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, TokenProvider.BEARER + accessToken));
+
+    // then
+    actions
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data").isArray())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].id").isNumber())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].name").isString())
+        .andExpect(
+            MockMvcResultMatchers.jsonPath("$.data[?(@.id == 1)].name").value(CATEGORY_NAME));
   }
 }
