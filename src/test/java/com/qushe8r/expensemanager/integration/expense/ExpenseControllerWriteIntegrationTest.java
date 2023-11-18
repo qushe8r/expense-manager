224ca0acde8b656d820f8daf42afd60af085753b
qushe8r <qushe8r@gmail.com>
2023-11-18 10:59:34 +0900

    Test: PATCH /expenses/{expenseId} 통합 테스트

--- src/test/java/com/qushe8r/expensemanager/integration/expense/ExpenseControllerWriteIntegrationTest.java
+++ src/test/java/com/qushe8r/expensemanager/integration/expense/ExpenseControllerWriteIntegrationTest.java
@@ -2,11 +2,13 @@ package com.qushe8r.expensemanager.integration.expense;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qushe8r.expensemanager.config.DataSourceSelector;
+import com.qushe8r.expensemanager.expense.dto.PatchExpense;
 import com.qushe8r.expensemanager.expense.dto.PostExpense;
 import com.qushe8r.expensemanager.security.jwt.JwtProperties;
 import com.qushe8r.expensemanager.security.jwt.TokenProvider;
 import com.qushe8r.expensemanager.stub.JwtFactory;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,6 +30,8 @@ import org.springframework.transaction.annotation.Transactional;
 class ExpenseControllerWriteIntegrationTest {
 
   private static final String EXPENSE_DEFAULT_URL = "/expenses";
+
+  private static final String EXPENSE_PATH_PARAMETER = "/{expenseId}";
 
   @Autowired private MockMvc mockMvc;
 
@@ -102,6 +106,117 @@ class ExpenseControllerWriteIntegrationTest {
         .andExpect(MockMvcResultMatchers.jsonPath("$.errorCode").value("MX01"))
         .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(404))
         .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("존재하지 않는 카테고리입니다."))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.fieldErrors").isEmpty())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.violationErrors").isEmpty());
+  }
+
+  @DisplayName("modifyExpense(): 입력값이 유효하면 성공한다.")
+  @Test
+  void modifyExpense() throws Exception {
+    // given
+    String accessToken = JwtFactory.withDefaultValues().generateToken(jwtProperties);
+
+    Long expenseId = 1L;
+
+    Long amount = 10000L;
+    String memo = "돼지국밥";
+    LocalDateTime expenseAt = LocalDateTime.of(2023, 11, 15, 12, 0);
+    Long categoryId = 1L;
+
+    PatchExpense patchExpense = new PatchExpense(amount, memo, expenseAt, categoryId);
+    String content = objectMapper.writeValueAsString(patchExpense);
+
+    // when
+    ResultActions actions =
+        mockMvc.perform(
+            MockMvcRequestBuilders.patch(EXPENSE_DEFAULT_URL + EXPENSE_PATH_PARAMETER, expenseId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, TokenProvider.BEARER + accessToken)
+                .content(content));
+
+    // then
+    actions
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data").isMap())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data.expenseId").isNumber())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data.amount").value(amount))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data.memo").value(memo))
+        .andExpect(
+            MockMvcResultMatchers.jsonPath("$.data.expenseAt")
+                .value(expenseAt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"))))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data.categoryName").value("카테고리"));
+  }
+
+  @DisplayName("modifyExpense(): 입력값이 유효하면 성공한다.")
+  @Test
+  void modifyExpenseCategoryNotFoundException() throws Exception {
+    // given
+    String accessToken = JwtFactory.withDefaultValues().generateToken(jwtProperties);
+
+    Long expenseId = 1L;
+
+    Long amount = 10000L;
+    String memo = "돼지국밥";
+    LocalDateTime expenseAt = LocalDateTime.of(2023, 11, 15, 12, 0);
+    Long categoryId = Long.MAX_VALUE;
+
+    PatchExpense patchExpense = new PatchExpense(amount, memo, expenseAt, categoryId);
+    String content = objectMapper.writeValueAsString(patchExpense);
+
+    // when
+    ResultActions actions =
+        mockMvc.perform(
+            MockMvcRequestBuilders.patch(EXPENSE_DEFAULT_URL + EXPENSE_PATH_PARAMETER, expenseId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, TokenProvider.BEARER + accessToken)
+                .content(content));
+
+    // then
+    actions
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(MockMvcResultMatchers.status().isNotFound())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.errorCode").value("MX01"))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(404))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("존재하지 않는 카테고리입니다."))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.fieldErrors").isEmpty())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.violationErrors").isEmpty());
+  }
+
+  @DisplayName("modifyExpense(): 입력값이 유효하면 성공한다.")
+  @Test
+  void modifyExpenseExpenseNotFoundException() throws Exception {
+    // given
+    String accessToken = JwtFactory.withDefaultValues().generateToken(jwtProperties);
+
+    Long expenseId = Long.MAX_VALUE;
+
+    Long amount = 10000L;
+    String memo = "돼지국밥";
+    LocalDateTime expenseAt = LocalDateTime.of(2023, 11, 15, 12, 0);
+    Long categoryId = 1L;
+
+    PatchExpense patchExpense = new PatchExpense(amount, memo, expenseAt, categoryId);
+    String content = objectMapper.writeValueAsString(patchExpense);
+
+    // when
+    ResultActions actions =
+        mockMvc.perform(
+            MockMvcRequestBuilders.patch(EXPENSE_DEFAULT_URL + EXPENSE_PATH_PARAMETER, expenseId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, TokenProvider.BEARER + accessToken)
+                .content(content));
+
+    // then
+    actions
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(MockMvcResultMatchers.status().isNotFound())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.errorCode").value("EX01"))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(404))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("예산을 찾을 수 없습니다."))
         .andExpect(MockMvcResultMatchers.jsonPath("$.fieldErrors").isEmpty())
         .andExpect(MockMvcResultMatchers.jsonPath("$.violationErrors").isEmpty());
   }
