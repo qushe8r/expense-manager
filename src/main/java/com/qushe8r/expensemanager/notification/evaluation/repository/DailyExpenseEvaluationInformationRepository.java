8a5e594b022d52d85dabd3a9e4b15e77dea643a4
qushe8r <qushe8r@gmail.com>
2023-11-26 10:09:22 +0900

    Feat: 지출이 없어서 알림에서 제외되는 것 수정

--- src/main/java/com/qushe8r/expensemanager/notification/evaluation/repository/DailyExpenseEvaluationInformationRepository.java
+++ src/main/java/com/qushe8r/expensemanager/notification/evaluation/repository/DailyExpenseEvaluationInformationRepository.java
@@ -23,8 +23,15 @@ public class DailyExpenseEvaluationInformationRepository {
     return entityManager
         .createQuery(
             """
-            SELECT new com.qushe8r.expensemanager.notification.evaluation.dto.DailyExpenseEvaluationInformation(t1.categoryName, t1.previousTotals, t1.todayTotals, b.amount)
-            FROM (SELECT c.name as categoryName,
+            SELECT
+            new com.qushe8r.expensemanager.notification.evaluation.dto.
+            DailyExpenseEvaluationInformation(
+            b.memberCategory.category.name,
+            ifnull(t1.previousTotals, 0),
+            ifnull(t1.todayTotals, 0),
+            b.amount)
+            FROM (
+            SELECT c.name as categoryName,
             SUM(CASE WHEN(e.expenseAt BETWEEN :firstDay AND :todayStart AND mc.member.id = :memberId) THEN e.amount ELSE 0 END) as previousTotals,
             SUM(CASE WHEN(e.expenseAt BETWEEN :todayStart AND :now AND mc.member.id = :memberId) THEN e.amount ELSE 0 END) as todayTotals,
             mc.id as member_category_id
@@ -32,9 +39,11 @@ public class DailyExpenseEvaluationInformationRepository {
             JOIN Category c ON c.id = mc.category.id
             JOIN Expense e ON e.memberCategory.id = mc.id
             WHERE mc.member.id = :memberId
-            GROUP BY mc.id) as t1
-            JOIN Budget b ON t1.member_category_id = b.memberCategory.id
+            GROUP BY mc.id
+            ) as t1
+            RIGHT JOIN Budget b ON t1.member_category_id = b.memberCategory.id
             WHERE b.month = :month
+            AND b.memberCategory.member.id = :memberId
             """,
             DailyExpenseEvaluationInformation.class)
         .setParameter("firstDay", firstDay)
