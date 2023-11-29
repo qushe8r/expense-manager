d1f33ae5aa58d76eb46d80345cc5e0ebfd97199f
qushe8r <qushe8r@gmail.com>
2023-11-29 17:33:35 +0900

    Docs: POST /reissue

--- src/test/java/com/qushe8r/expensemanager/security/controller/AuthControllerTest.java
+++ src/test/java/com/qushe8r/expensemanager/security/controller/AuthControllerTest.java
@@ -10,10 +10,17 @@ import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
+import org.springframework.restdocs.cookies.CookieDocumentation;
+import org.springframework.restdocs.headers.HeaderDocumentation;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -21,11 +28,14 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTestWithoutSecurityConfig(AuthController.class)
+@AutoConfigureRestDocs
 class AuthControllerTest {
 
-  @Autowired private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-  @MockBean private AuthService authService;
+  @MockBean
+  private AuthService authService;
 
   @Test
   void reissue() throws Exception {
@@ -45,7 +55,7 @@ class AuthControllerTest {
     // when
     ResultActions actions =
         mockMvc.perform(
-            MockMvcRequestBuilders.post("/reissue")
+            RestDocumentationRequestBuilders.post("/reissue")
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .cookie(cookie));
@@ -55,6 +65,20 @@ class AuthControllerTest {
         .andDo(MockMvcResultHandlers.print())
         .andExpect(MockMvcResultMatchers.status().isOk())
         .andExpect(MockMvcResultMatchers.header().string(HttpHeaders.AUTHORIZATION, newAccessToken))
-        .andExpect(MockMvcResultMatchers.header().exists(HttpHeaders.SET_COOKIE));
+        .andExpect(MockMvcResultMatchers.header().exists(HttpHeaders.SET_COOKIE))
+        .andDo(MockMvcRestDocumentation.document(
+            "post-reissue",
+            Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+            Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+            CookieDocumentation.requestCookies(
+                CookieDocumentation.cookieWithName("Refresh").description("리프레시 토큰")
+            ),
+            HeaderDocumentation.responseHeaders(
+                HeaderDocumentation.headerWithName(HttpHeaders.AUTHORIZATION).description("액세스 토큰(재발급)")
+            ),
+            CookieDocumentation.responseCookies(
+                CookieDocumentation.cookieWithName("Refresh").description("리프레시 토큰(재발급)")
+            )
+        ));
   }
 }
